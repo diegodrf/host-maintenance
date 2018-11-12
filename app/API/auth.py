bc0c7a24053891de8445291d7176558c20c18e63
Diego Faria <diego.rdfaria@gmail.com>
2018-11-12 17:51:36 -0200

    Delete auth.py

--- app/API/auth.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Auth:
-    # Variáveis de ambiente para Docker
-    zabbixServer = 'http://10.241.0.4/zabbix' #os.getenv('ZABBIX_SERVER', 'http://0.0.0.0')
-    zabbixUser = 'diego.rodrigues' #os.getenv('ZABBIX_USER', 'Admin')
-    zabbixPassword = 'Curumim982150*' #os.getenv('ZABBIX_PASSWORD', 'zabbix')
