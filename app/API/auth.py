4850d99e2981781a74e7adeeff3d0a2c11b55b5b
Diego Rodrigues <diego.rdfaria@gmail.com>
2018-11-26 15:34:34 -0200

    Removido o sistema de login. Conversão de horário na lista de manutenções está correto.

--- app/API/auth.py
+++ app/API/auth.py
@@ -3,6 +3,6 @@ import os
 
 class Auth:
     # Variáveis de ambiente para Docker
-    zabbixServer = 'http://docker.westus2.cloudapp.azure.com' #os.getenv('ZABBIX_SERVER', 'http://0.0.0.0')
+    zabbixServer = os.getenv('ZABBIX_SERVER', 'http://0.0.0.0')
     zabbixUser = os.getenv('ZABBIX_USER', 'Admin')
     zabbixPassword = os.getenv('ZABBIX_PASSWORD', 'zabbix')
--- app/main.py
+++ app/main.py
@@ -1,38 +1,33 @@
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, url_for, request
 import app.epoch as epoch
 from ZabbixAPI_py import Zabbix
 from app import models
 from app.API.auth import Auth
-from time import time, sleep
+import time
+
 
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
-app.secret_key = 'Rn5!c3cU@a5t'
+
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date):
+    return time.strftime('%d/%m/%Y - %H:%M', time.localtime(int(date)))
+
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('maintenance'))
 
 
-@app.route('/maintenance/<user>')
-def maintenance(user):
-
-    # Verifica se o usuário está logado a mais de 10 minutos. Se sim, da timeout e desloga, se não, permite o acesso
-    # e renova o timestamp
-    if round(time() - session[user]['timestamp'], 0) > 600.0:
-        session.pop(user, None)
-        return redirect(url_for('login'))
-    else:
-        session[user]['timestamp'] = time()
-
+@app.route('/maintenance', methods=['GET', 'POST'])
+def maintenance():
+    if request.method == 'GET':
         # Faz um get para listar os hosts para o usuário. Só será retornado o que o usuário tiver permissão para ver.
         api = Zabbix(server=Auth.zabbixServer)
-        api.login(user=session[user]['user'], password=session[user]['password'])
+        api.login(user=Auth.zabbixUser, password=Auth.zabbixPassword)
         hosts = [host for host in api.host('get', {'output': ['hostid', 'name']})]
 
         return render_template('maintenance.html', hosts=hosts)
-
-@app.route('/manitenanceToZabbix', methods=['POST'])
-def maintenanceToZabbix():
 
     # Recebe os campos do POST
     name = request.form['name']
@@ -62,8 +57,6 @@ def maintenanceToZabbix():
     #     message = maintenanceZabbix.create(ids=host_list, groupids=True)
 
     message = maintenanceZabbix.create(ids=host_list, groupids=False)
-    print(message)
-    print(message['maintenanceids'][0])
     maintenance_created = models.get_maintenance(message['maintenanceids'][0])
 
     return render_template('maintenance_created.html', maintenance=maintenance_created)
@@ -71,37 +64,9 @@ def maintenanceToZabbix():
 
 @app.route('/maintenance_list')
 def maintenance_list():
-    try:
-        maintenances = models.get_maintenance()
-        return render_template('maintenance_list.html', maintenances=maintenances)
-    except:
-        return redirect('login')
 
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/auth', methods=['POST'])
-def auth():
-    user = request.form['user']
-    password = request.form['password']
-    timestamp = time()
-    server = Auth.zabbixServer
-
-    # Verifica se o usuário já possui sessão ativa, se tiver, permite a entrada e renova seu timestamp,
-    # se não, verifica no zabbix a existência do usuário.
-    if user in session:
-        session[user]['timestamp'] = time()
-        return redirect(url_for('maintenance', user=user))
-    else:
-        api = Zabbix(server=server)
-        token = api.login(user=user, password=password)
-        if len(token) == 32:
-            session[user] = {'user': user, 'password': password, 'timestamp': timestamp}
-            return redirect(url_for('maintenance', user=user))
-        else:
-            return redirect(url_for('login'))
+    maintenances = models.get_maintenance()
+    return render_template('maintenance_list.html', maintenances=maintenances)
 
 
 if __name__ == '__main__':
